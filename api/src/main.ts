dff984ed3f996d63812e991299a6e1e899efe8b0
Rayyan <rayyan808@gmail.com>
2026-04-15 17:01:20 +0400

    bootstrap NestJS app on port 3000 with CORS for Vite dev server

---
+++ api/src/main.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: 'http://localhost:5173' });
+  await app.listen(3000);
+  console.log('MNA Agent API running on http://localhost:3000');
+}
+bootstrap();
