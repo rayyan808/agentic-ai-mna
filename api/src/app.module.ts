2fc35d23489b77707617ef6bc146f91e406317a8
Rayyan <rayyan808@gmail.com>
2026-04-21 18:47:40 +0400

    add db stubs

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from "@nestjs/common";
 import { ChatModule } from "./chat/chat.module";
 import { ListenerModule } from "./listener/listener.module";
-
+import { TypeOrmModule } from "@nestjs/typeorm";
 @Module({
-  imports: [ChatModule, ListenerModule],
+  imports: [
+    ChatModule,
+    ListenerModule,
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT) ?? 5163,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+    }),
+  ],
 })
 export class AppModule {}
