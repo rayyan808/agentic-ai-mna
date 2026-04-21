7e287165e4df505f84a97848ced851d089f632c9
Rayyan <rayyan808@gmail.com>
2026-04-22 01:06:15 +0400

    asset service update

---
+++ api/src/assets/assets.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { asset_info } from "src/listener/listener.constants";
+import { AssetInfo } from "./entities/asset.entity";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class AssetService {
+  constructor(
+    @InjectRepository(AssetInfo) private assetRepo: Repository<AssetInfo>,
+  ) {}
+
+  async findAssetInfo(asset_name: string) {}
+
+  async updateAsset(
+    assetName: string,
+    currency: string,
+    assetInfo: asset_info,
+  ) {
+    console.log(`Updating asset ${assetName}`);
+    const row = await this.assetRepo.findOne({
+      where: { asset_name: assetName, currency: currency },
+    });
+    if (row) {
+      //exists
+      row.summed_price += assetInfo.summed_price;
+      row.summed_units += assetInfo.summed_units;
+      await this.assetRepo.save(row);
+      console.log(`New TWAP: ${row.summed_price / row.summed_units}`);
+    } else {
+      console.log(`Asset doesnt exist, creating..`);
+      await this.assetRepo.insert({
+        asset_name: assetName,
+        currency: currency,
+        summed_price: assetInfo.summed_price,
+        summed_units: assetInfo.summed_units,
+      });
+    }
+  }
+}
