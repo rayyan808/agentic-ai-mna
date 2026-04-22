920094d963d2faccd7e110af76bc199f40014f66
Rayyan <rayyan808@gmail.com>
2026-04-23 01:03:04 +0400

    move sale_record module on layer up

--- api/src/sale_record/sale_record.module.ts
+++ api/src/sale_record/sale_record.module.ts
@@ -1,7 +1,10 @@
 import { Module } from "@nestjs/common";
 import { SaleRecordService } from "./sale_record.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { SaleRecord } from "./sale.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([SaleRecord])],
   providers: [SaleRecordService],
   exports: [SaleRecordService],
 })
