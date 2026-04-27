274901c38a2625308835ed8ae000ec08bdd5049b
Rayyan <rayyan808@gmail.com>
2026-04-27 17:58:40 +0500

    getMovingAverage time bucket query

--- api/src/sale_record/sale_record.dto.ts
+++ api/src/sale_record/sale_record.dto.ts
@@ -5,3 +5,17 @@ export interface Sale {
   units: number;
   timestamp: number;
 }
+
+export enum TimePeriod {
+  hour = "1 hour",
+  six_hour = "6 hours",
+  day = "1 day",
+  week = "7 days",
+  two_week = "2 weeks",
+  month = "1 month",
+}
+
+export interface MovingAverage {
+  bucket: string;
+  avg_price: string;
+}
--- api/src/sale_record/sale_record.service.ts
+++ api/src/sale_record/sale_record.service.ts
@@ -1,5 +1,5 @@
-import { MoreThan, QueryBuilder, Repository } from "typeorm";
-import { Sale } from "./sale_record.dto";
+import { Repository } from "typeorm";
+import { MovingAverage, Sale, TimePeriod } from "./sale_record.dto";
 import { SaleRecord } from "src/sale_record/sale.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 
@@ -13,12 +13,39 @@ export class SaleRecordService {
     });
     return;
   }
-  //@TODO: Convert to a real-time aggregation
-  async getRecords(fromDate: number, toDate: number): Promise<SaleRecord[]> {
-    const x = this.saleRepo.query<SaleRecord[]>(
+  async getMovingAverage(
+    asset_name: string,
+    token_name: string,
+    bucket_size: TimePeriod,
+    time_window: TimePeriod,
+  ): Promise<MovingAverage[]> {
+    const x = await this.saleRepo.query<MovingAverage[]>(
       `
       SELECT 
+        time_bucket($1::interval, s.timestamp) as bucket,
+        locf(SUM(s.price) / NULLIF(SUM(s.units), 0)) as avg_price
+      FROM sale_record s
+      WHERE s.timestamp > now() - $1::interval
+        AND s.asset_name = $2
+        AND s.token_name = $3
+      GROUP BY bucket
+      ORDER BY bucket
       `,
+      [bucket_size, time_window, asset_name, token_name],
+    );
+    console.log(`Got bucket: ${JSON.stringify(x, null, 3)}`);
+    return x;
+  }
+  async getRecords(fromDate: Date, toDate: Date | null): Promise<SaleRecord[]> {
+    const x = this.saleRepo.query(
+      `
+      SELECT * 
+      FROM sale_record s
+      WHERE s.timestamp > $1
+        AND ($2 is NULL) OR (s.timestamp < $2)
+      ORDER BY timestamp ASC
+      `,
+      [fromDate, toDate],
     );
     return x;
   }
