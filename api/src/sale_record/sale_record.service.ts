b2312b317f3624ed28b09bd8bc60497e22dab0cb
Rayyan <rayyan808@gmail.com>
2026-04-29 01:58:52 +0500

    fix adding decimals

--- api/src/sale_record/sale_record.service.ts
+++ api/src/sale_record/sale_record.service.ts
@@ -2,6 +2,7 @@ import { Repository } from "typeorm";
 import { Candlestick, Sale, TradeWindow } from "./sale_record.dto";
 import { SaleRecord } from "src/sale_record/sale.entity";
 import { InjectRepository } from "@nestjs/typeorm";
+import Decimal from "decimal.js";
 
 export class SaleRecordService {
   constructor(
@@ -25,9 +26,9 @@ export class SaleRecordService {
     console.log(
       `Getting ${asset_name}/${token_name} from: ${from} to: ${to} ${tradeWindow.valueOf()}`,
     );
-    return await this.saleRepo.query<Candlestick[]>(
+    const rows = await this.saleRepo.query(
       `
-      SELECT 
+      SELECT
         bucket,
         asset_name,
         token_name,
@@ -42,7 +43,7 @@ export class SaleRecordService {
         sum_price_volume / volume as vwap,
         sum_price / trade_count as avg_price
       FROM ${tradeWindow.valueOf()}
-      WHERE 
+      WHERE
         bucket >= $3
         AND bucket < $4
         AND asset_name = $1
@@ -51,6 +52,17 @@ export class SaleRecordService {
     `,
       [asset_name, token_name, from, to],
     );
+    return rows.map((row: any): Candlestick => ({
+      ...row,
+      open: new Decimal(row.open),
+      close: new Decimal(row.close),
+      low: new Decimal(row.low),
+      high: new Decimal(row.high),
+      sum_price: new Decimal(row.sum_price),
+      sum_price_volume: new Decimal(row.sum_price_volume),
+      vwap: new Decimal(row.vwap),
+      avg_price: new Decimal(row.avg_price),
+    }));
   }
   async getRecords(fromDate: Date, toDate: Date): Promise<SaleRecord[]> {
     const x = this.saleRepo.query(
