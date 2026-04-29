891e1667c624ff0e0838f1aaf3fdce30aee990d5
Rayyan <rayyan808@gmail.com>
2026-04-29 21:36:59 +0500

    Add Aave Listener Config table

--- api/src/evm/protocols/aavev3.module.ts
+++ api/src/evm/protocols/aavev3.module.ts
@@ -1,7 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AaveV3Service } from "./aavev3.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AaveV3Config } from "./aavev3.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AaveV3Config])],
   providers: [AaveV3Service],
 })
 export class AaveProtocolModule {}
