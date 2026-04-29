35d850744a2d76d0bdf058dc13e42afe407d6c92
Rayyan <rayyan808@gmail.com>
2026-04-29 21:43:58 +0500

    Add cross-chain configs for AAVE V3

--- api/src/evm/protocols/aavev3.service.ts
+++ api/src/evm/protocols/aavev3.service.ts
@@ -1,1 +1,34 @@
-export class AaveV3Service {}
+import { InjectRepository } from "@nestjs/typeorm";
+import { AaveV3Config } from "./aavev3.entity";
+import { Repository } from "typeorm";
+import { ChainConfigs } from "./aavev3.config";
+
+export class AaveV3Service {
+  constructor(
+    @InjectRepository(AaveV3Config)
+    private configService: Repository<AaveV3Config>,
+  ) {}
+
+  async getLastProcessedBlock(chain: string): Promise<number> {
+    try {
+      const block = await this.configService.findOneOrFail({
+        where: {
+          chain,
+        },
+      });
+      return block.lastProcessedBlock;
+    } catch (e) {
+      console.log(
+        `No blocks processed yet for ${chain} on AAVE V3. Creating config..`,
+      );
+      const config = ChainConfigs[chain];
+      await this.configService.insert([
+        {
+          chain: config.chain,
+          lastProcessedBlock: config.startBlock,
+        },
+      ]);
+      return config.startBlock;
+    }
+  }
+}
