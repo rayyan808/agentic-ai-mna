347741863e49bb29dd0f33baafd2556f59a3a18e
Rayyan <rayyan808@gmail.com>
2026-05-02 02:41:01 +0500

    open cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:5173' });
+  app.enableCors({ origin: true });
   await app.listen(3000);
   console.log('MNA Agent API running on http://localhost:3000');
 }
